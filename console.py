75684686fdca414965232d45c5711ce7af794b51
AKoth-Otieno <elsyakoth@gmail.com>
2022-10-27 04:33:14 -0700

    add prompt.

--- console.py
+++ console.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python3
-"""The entry point to the command-line interpreter.
+"""The entry point to the command-line interpreter - cnsole for the airbnb clone.
 It implements:
-    quit and EOF to exit the program
-    help (this action is provided by default by cmd but you should keep it updated and documented as you work through tasks)
-    a custom prompt: (hbnb)
-    an empty line + ENTER shouldn’t execute anything
+   - quit and EOF to exit the program
+   - help (this action is provided by default by cmd but you should keep it updated and documented as you work through tasks)
+   - a custom prompt: (hbnb)
+   - an empty line + ENTER shouldn’t execute anything
 """
 
 import cmd
+import sys
+import argparser
+from from models.base_model import BaseModel
+
 
 class HBNBCommand(cmd.Cmd):
+    """HBNBCommand is the console for the airbnb clone.
+    Attributes:
+        prompt (str): The command prompt.
+    """
+     prompt = '(hbnb) '
+     __file_path = 'file.json'
+
+
+
 
 
 
