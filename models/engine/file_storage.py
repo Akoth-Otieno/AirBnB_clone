c296f4dc365729c0f19d389a8981130a1ba671c8
Mensire <mensirer@gmail.com>
2022-10-30 22:44:43 +0300

    storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,45 +1,40 @@
 #!/usr/bin/python3
-''' 
-Custom FileStorage Class.
-'''
+"""Defines the FileStorage class."""
 import json
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
-from models.amenity import Amenity
 from models.place import Place
+from models.amenity import Amenity
 from models.review import Review
-import os.path
+
 
 class FileStorage:
-    '''
-    Represents a storage engine.
-
+    """Represent an abstracted storage engine.
     Attributes:
         __file_path (str): The name of the file to save objects to.
         __objects (dict): A dictionary of instantiated objects.
-    '''
-    def __init__(self):
-        '''INitializes File Storage Instances.'''
-        self.__file_path = "file.json"
-        self.__objects = {}
+    """
+    __file_path = "file.json"
+    __objects = {}
 
     def all(self):
-        '''returns all objects.'''
-        return (self.__objects)
+        """Return the dictionary __objects."""
+        return FileStorage.__objects
 
     def new(self, obj):
-        '''creates new objects.'''
-        new_id = getattr(obj, 'id')
-        self.__objects[obj.to_dict()['__class__'] + '.' + new_id] = obj
+        """Set in __objects obj with key <obj_class_name>.id"""
+        ocname = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
+
     def save(self):
         """Serialize __objects to the JSON file __file_path."""
-        obj_dict = FileStorage.__objects
-        obj_dict = {obj: odict[obj].to_dict() for obj in odict.keys()}
+        odict = FileStorage.__objects
+        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
         with open(FileStorage.__file_path, "w") as f:
             json.dump(objdict, f)
-    
+
     def reload(self):
         """Deserialize the JSON file __file_path to __objects, if it exists."""
         try:
