81ffad5875ca969aff1f04463829266d3cc76085
AKoth-Otieno <elsyakoth@gmail.com>
2022-10-30 10:30:37 -0700

    create a class State that inherits from BaseModel.

---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Defines a class State that inherits from Basemodel.'''
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    '''Represents a users State of residence.'''
+    name = ''
